f17f6e29d0588997c0b8a509033073d750a90a52
xviiivin <88417506+xviiivin@users.noreply.github.com>
2023-03-03 10:43:42 +0700

    :key in userslip

--- client/src/views/userSlip.vue
+++ client/src/views/userSlip.vue
@@ -4,7 +4,7 @@
             <div class="mx-w-5xl mt-[40px]">
                   <div class="container mx-auto  mt-[50px] flex grid grid-cols-5 gap-3 gap-y-[30px]">
                         <div class="rounded-3xl w-[200px] h-[250px] bg-white drop-shadow-xl mx-[20px]"
-                              v-for="val in allslip ">
+                              v-for="(val,index) in allslip " :key="index">
                               <div class="text-justify justify-center p-5">
                                     <img class="py-4 h-full w-full" :src="val.slipp">
                               </div>
