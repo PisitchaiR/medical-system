cd806490426ced8e9ceb25fa38c02411d7b56bb7
Wiranyupaa <64070231@kmitl.ac.th>
2023-03-03 13:15:24 +0700

    doc info layout

--- client/src/views/docInfo.vue
+++ client/src/views/docInfo.vue
@@ -3,32 +3,31 @@
 <template>
   <div>
     <Nav />
-    <div class="mx-w-5xl mb-[200px]">
-      <img src="../assets/docInfo-image.png" class="h-full w-full" />
-      <div
-        class="absolute right-[200px] top-[200px] h-[250px] w-[600px] text-[#111727] flex justify-center"
-      >
-        <p class="absolute text-3xl pt-[20px] font-bold">
-          นางสาววิรัลยุพา เพ็ชรอินทร์
-        </p>
-        <p class="absolute text-lg text-[#949494] font-light pt-[80px]">
-          โรงพยาบาลW&N
-        </p>
-        <p class="absolute text-xl pt-[140px] font-semibold">สาขาที่เชียวชาญ</p>
-        <ol
-          class="text-md font-medium pt-[180px] pl-[100px] list-outside list-disc"
-        >
-          <li class="text-red-500">
-            <span class="text-[#111727]"> สาขาออร์โธปิดิกส์</span>
-          </li>
-          <li class="text-red-500">
-            <span class="text-[#111727]"> อนุสาขาโรคของกระดูกสันหลัง</span>
-          </li>
-        </ol>
+    
+      <div class="mx-w-5xl mb-[200px] mt-[40px]">
+        <img src="../assets/docInfo-image.png" class="h-full w-full" />
+        <div class="absolute right-[200px] top-[200px] h-[250px] w-[600px] text-[#111727] flex justify-center">
+          <p class="absolute text-3xl pt-[20px] font-bold">
+            นางสาววิรัลยุพา เพ็ชรอินทร์
+          </p>
+          <p class="absolute text-lg text-[#949494] font-light pt-[80px]">
+            โรงพยาบาลW&N
+          </p>
+          <p class="absolute text-xl pt-[140px] font-semibold">สาขาที่เชียวชาญ</p>
+          <ol class="text-md font-medium pt-[180px] pl-[100px] list-outside list-disc">
+            <li class="text-red-500">
+              <span class="text-[#111727]"> สาขาออร์โธปิดิกส์</span>
+            </li>
+            <li class="text-red-500">
+              <span class="text-[#111727]"> อนุสาขาโรคของกระดูกสันหลัง</span>
+            </li>
+          </ol>
+        </div>
       </div>
+      <div class="container mx-auto mt-[50px]">
+      <preBan />
+      <conTac />
     </div>
-    <preBan />
-    <conTac />
   </div>
 </template>
 
